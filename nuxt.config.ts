d55facd53dff45fcaf5eb7d8e85b2ac950019657
Alain Cyr <alain.cyr@gmail.com>
2025-09-08 00:52:45 -0400

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,22 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  pages: true
+  pages: true,
+
+  // Nitro configuration for server
+  nitro: {
+    experimental: {
+      wasm: true,
+    },
+    preset: 'netlify',
+    prerender: {
+      // Disable prerender during build to avoid hitting runtime endpoints or assets
+      crawlLinks: false,
+      routes: [],
+      ignore: ['/**'],
+      failOnError: false
+    }
+  },
+
+
 })
