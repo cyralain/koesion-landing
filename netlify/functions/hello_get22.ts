068b6284daf22a019b4a2b9b47675e58fc311e25
Alain Cyr <alain.cyr@gmail.com>
2025-09-12 20:26:15 -0400

    test functions

--- netlify/functions/hello_get22.ts
+++ netlify/functions/hello_get22.ts
@@ -1,7 +1,12 @@
-export default defineEventHandler(() => {
+import { Handler } from '@netlify/functions';
+
+const handler: Handler = async (event, context) => {
   return {
-    message: 'hello nice to meet you 22'
-  }
-})
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World from Netlify Function!22" }),
+  };
+};
+
+export { handler };
 
 
