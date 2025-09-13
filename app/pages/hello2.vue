b5c7789e6834455cfc4cec4acf767328b1a33427
Alain Cyr <alain.cyr@gmail.com>
2025-09-12 20:45:32 -0400

    test page api

--- app/pages/hello2.vue
+++ app/pages/hello2.vue
@@ -6,8 +6,11 @@
 </template>
 
 <script setup lang="ts">
-// const { data } = await useFetch('/api/hello', { key: 'hello2-app-pages' })
-const { data } = await useFetch('/.netlify/functions/hello_get22', { key: 'hello2-app-pages' })
+  try {
+    // const { data } = await useFetch('/api/hello', { key: 'hello2-app-pages' })
+      const { data } = await useFetch('/.netlify/functions/hello_get22')
+  } catch (error) {
+      console.error('Error fetching function:', error);
+  }
 </script>
-
 
